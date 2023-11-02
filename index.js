c59927a7e64e55d51c3f73b87a4e70928d2163b7
Alexandre Pacheco <76924249+AlexandrePacheco-Oz@users.noreply.github.com>
2023-11-01 22:20:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let chave = prompt(alert("Qual é a palavra para sair?? "));
+
+
+
+while(true){
+    if(chave == "sair"){
+        alert("Você saiu");
+        break;
+
+    }else{
+        chave =  prompt(alert("Qual é a palavra para sair?? "));
+    }
+  
+}
